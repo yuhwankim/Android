8fc9c91db533a2773abb0f5932d889c41ce393fd
kwanulee <kwlee@hansung.ac.kr>
2017-09-21 10:34:04 +0900

    ActivityIntent 코드 예제 추가

---
+++ examples/ActivityIntent/app/src/main/java/com/jyheo/activityintent/SecondActivity.java
@@ -0,0 +1,55 @@
+package com.jyheo.activityintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "ActivityLifeCycle";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.i(TAG, getLocalClassName() + ".onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, getLocalClassName() + ".onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, getLocalClassName() + ".onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, getLocalClassName() + ".onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, getLocalClassName() + ".onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, getLocalClassName() + ".onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, getLocalClassName() + ".onDestroy");
+    }
+
+
+}
