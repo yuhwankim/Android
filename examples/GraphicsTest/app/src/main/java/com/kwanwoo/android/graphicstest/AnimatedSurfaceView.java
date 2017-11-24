63b444b3048009d65f8f8fc0d5a1c4db6ab995c4
kwanulee <kwlee@hansung.ac.kr>
2017-11-24 11:49:52 +0900

    GraphicsTest 예제 추가

---
+++ examples/GraphicsTest/app/src/main/java/com/kwanwoo/android/graphicstest/AnimatedSurfaceView.java
@@ -0,0 +1,71 @@
+package com.kwanwoo.android.graphicstest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+
+public class AnimatedSurfaceView  extends SurfaceView implements SurfaceHolder.Callback {
+
+    private BallAnimation thread;
+    private ArrayList<Ball> arBall = new ArrayList<Ball>();
+
+    SurfaceHolder holder;
+
+
+    public AnimatedSurfaceView(Context context) {
+        super(context);
+        holder = getHolder();
+        holder.addCallback(this);
+
+        thread = new BallAnimation();
+    }
+
+
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        thread.start();
+    }
+
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {}
+
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {}
+    }
+
+    class BallAnimation extends Thread {
+        public void run() {
+            while (true) {
+                Canvas canvas  = holder.lockCanvas(null);
+
+                canvas.drawColor(Color.WHITE);
+                synchronized (holder) {
+                    for (int idx=0; idx<arBall.size(); idx++) {
+                        Ball B = arBall.get(idx);
+                        B.move(getWidth(),getHeight());
+                        B.draw(canvas);
+                    }
+                }
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            int x =  (int)event.getX();
+            int y =  (int)event.getY();
+
+            arBall.add(new Ball(x, y));
+
+            return true;
+        }
+        return false;
+    }
+}
