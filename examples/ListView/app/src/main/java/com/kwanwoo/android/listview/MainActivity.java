228056a7beb7f29a81aeee9b48307d3cfd78040b
kwanulee <kwlee@hansung.ac.kr>
2019-03-24 16:28:53 +0900

    gradle 3.3.0, api 28 컴파일

--- examples/ListView/app/src/main/java/com/kwanwoo/android/listview/MainActivity.java
+++ examples/ListView/app/src/main/java/com/kwanwoo/android/listview/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
         //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
         ArrayAdapter<String> adapt
                 = new ArrayAdapter<String>(
-                        this,
-                        android.R.layout.simple_list_item_1,
-                        items);
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    items);
         return adapt;
     }
 
@@ -50,13 +50,13 @@ public class MainActivity extends AppCompatActivity {
         //어댑터 준비 (리소스 배열 객체 이용, simple_list_item_1 리소스 사용
         ArrayAdapter<CharSequence> adapt
                 = ArrayAdapter.createFromResource(
-                                    this,
-                                    R.array.items,
-                                    android.R.layout.simple_list_item_1);
+                    this,
+                    R.array.items,
+                    android.R.layout.simple_list_item_1);
         return adapt;
     }
 
-    private ListAdapter createAdapter3()  {
+    private ListAdapter createAdapter3() {
 
         // 데이터 원본 준비
         String[] items = {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"};
@@ -64,29 +64,29 @@ public class MainActivity extends AppCompatActivity {
         //어댑터 준비 (배열 객체 이용, R.layout.item(사용자 정의) 리소스 사용
         ArrayAdapter<String> adapt
                 = new ArrayAdapter<String>(
-                this,
-                R.layout.item,
-                items);
+                    this,
+                    R.layout.item,
+                    items);
 
         return adapt;
     }
 
     private ListAdapter createAdapter4() {
         // 데이터 원본 준비
-        ArrayList<HashMap<String, String>> mlist = new ArrayList<HashMap<String, String>> ();
+        ArrayList<HashMap<String, String>> mlist = new ArrayList<HashMap<String, String>>();
 
-        HashMap<String, String> map = new HashMap<String, String> ();
+        HashMap<String, String> map = new HashMap<String, String>();
         map.put("name", "Kim");
         map.put("age", "22");
         mlist.add(map);
 
-        map = new HashMap<String, String> ();
+        map = new HashMap<String, String>();
         map.put("name", "Lee");
         map.put("age", "21");
 
         mlist.add(map);
 
-        map = new HashMap<String, String> ();
+        map = new HashMap<String, String>();
         map.put("name", "Park");
         map.put("age", "23");
 
