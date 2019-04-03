013d28b26f6e60d03246aa91e44400b73ab11991
kwanulee <kwlee@hansung.ac.kr>
2019-04-03 09:51:19 +0900

    ActivityIntent 프로젝트 업데이트

--- examples/ActivityIntent/app/src/main/java/com/example/activityintent/SecondActivity.java
+++ examples/ActivityIntent/app/src/main/java/com/example/activityintent/SecondActivity.java
@@ -1,30 +1,36 @@
-package com.kwanwoo.android.activityintent;
+package com.example.activityintent;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
-    private static final String TAG = "ActivityLifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+
         Log.i(TAG, getLocalClassName() + ".onCreate");
+        setContentView(R.layout.activity_second);
+
+        Button btn = findViewById(R.id.buttonSecondActivity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+    private static final String TAG = "Lifecycle";
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, getLocalClassName() + ".onStart");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(TAG, getLocalClassName() + ".onRestart");
     }
 
     @Override
@@ -46,10 +52,14 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, getLocalClassName() + ".onRestart");
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, getLocalClassName() + ".onDestroy");
     }
-
-
 }
