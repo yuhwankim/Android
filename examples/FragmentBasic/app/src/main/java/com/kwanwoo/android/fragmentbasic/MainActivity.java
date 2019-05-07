bf28504165a03a94ceb186204ef52c85c569ae29
kwanulee <kwlee@hansung.ac.kr>
2019-05-07 16:40:30 +0900

    FragmentBasic 업데이트

--- examples/FragmentBasic/app/src/main/java/com/kwanwoo/android/fragmentbasic/MainActivity.java
+++ examples/FragmentBasic/app/src/main/java/com/kwanwoo/android/fragmentbasic/MainActivity.java
@@ -1,11 +1,9 @@
 package com.kwanwoo.android.fragmentbasic;
 
-import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FragmentTransaction fragmentTransaction =
-                getSupportFragmentManager().beginTransaction();
-
-        fragmentTransaction.replace(R.id.container, new SecondFragment());
-
-        fragmentTransaction.addToBackStack(null);
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new FirstFragment());
         fragmentTransaction.commit();
     }
 }
